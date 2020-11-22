258e4396d9999499fe66f60b2ac2ec5ef0dabbf2
suomiy <suomiy@gmail.com>
2020-11-22 22:30:31 +0100

    remove SitesApp from TableScript

--- src/TableScript/Code.js
+++ src/TableScript/Code.js
@@ -1,15 +1,11 @@
 var allowedInstances = ['clients', 'events', 'employees', 'tarrifs', 'groups'];
-var allowedPages = ['sprava-klientu', 'sprava-udalosti', 'sprava-uzivatelu', 'sprava-cenovych-pasem', 'sprava-skupin'];
 
 var getInstance = function(e) {
   var instance = e.parameter.instance;
   if (instance) {
     return allowedInstances.indexOf(instance) >= 0 ? instance : '';
   }
-  var activePage = SitesApp.getActivePage();
-  var activePageName = activePage && activePage.getName();
-  var pageIdx = allowedPages.indexOf(activePageName);
-  return pageIdx >= 0 ? allowedInstances[pageIdx] : '';
+  return '';
 }
 
 function doGet(e) {
