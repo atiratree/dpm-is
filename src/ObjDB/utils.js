77cbc4a418a106d05356269d6fdf0fd277924c12
suomiy <suomiy@gmail.com>
2021-12-15 17:45:48 +0100

    enhance logging

--- src/ObjDB/utils.js
+++ src/ObjDB/utils.js
@@ -1,11 +1,27 @@
 function logException_(e) {
   var errSheet = SpreadsheetApp.openById('1DBFMQfLfXl1aWIotXDcGvRJZAVP8DJeOMwiTvdkm5Ko').getSheetByName('errors');
-  logMessage_(errSheet, e.name + ' in line ' + e.lineNumber + ': ' + e.message);
-  logMessage_(errSheet, e.stack.match(/\([^\)]*\)/g).reverse().join(' > ').replace(/[\(\)]/g, ''));
+  var user = "null"
+  var scriptName = "null"
+
+  try {
+    user = Session.getActiveUser().getEmail();
+  } catch(ignored) {
+  }
+
+  try {
+    var scriptID = ScriptApp.getScriptId();
+    scriptName = DriveApp.getFileById(scriptID).getName();
+  } catch(ignored) {
+  }
+
+
+  logMessage_(errSheet, user, scriptName, e.name + ' in line ' + e.lineNumber + ': ' + e.message);
+  logMessage_(errSheet, user, scriptName, e.stack.match(/\([^\)]*\)/g).reverse().join(' > ').replace(/[\(\)]/g, ''));
 }
 
-function logMessage_(sheet, message) {
-  var value = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1") + " : " + message;
+function logMessage_(sheet, user, scriptName, message) {
+  var date = new Date().toString().replace(/(.*\d{2}:\d{2}:\d{2}).*/, '$1')
+  var value = '[' + date + '] [' + user +  '] [' + scriptName + ']  ' + message;
   sheet.appendRow([value]);
 }
 
