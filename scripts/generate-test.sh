31326ebc1de42e97ba4ca1ebe9bed70c0979e66b
Filip Křepinský <atiratree@gmail.com>
2022-06-03 03:44:32 +0200

    introduce test and generate-test scripts

---
+++ scripts/generate-test.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+function visit() {
+  pushd "${1}" > /dev/null
+}
+
+function leave() {
+  popd > /dev/null
+}
+
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+REPO_DIR="$(realpath "${SCRIPT_DIR}/..")"
+
+for DIR_PATH in "${REPO_DIR}/__tests__"/*; do
+    DIR_NAME="`basename "${DIR_PATH}"`"
+    BUNDLE="${REPO_DIR}/__tests__/${DIR_NAME}/${DIR_NAME}-bundle.js"
+    cat "${REPO_DIR}/__tests__/${DIR_NAME}/mocks.js" > "${BUNDLE}"
+    cat "${REPO_DIR}/src/${DIR_NAME}"/*\.js | sed 's/^function/export function/g' >> "${BUNDLE}"
+done
