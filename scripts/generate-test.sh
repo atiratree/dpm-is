b815c97b91612882e847bc93a620ec953b9e16c6
Filip Křepinský <atiratree@gmail.com>
2023-04-12 16:53:34 +0200

    init Utils package for testing

--- scripts/generate-test.sh
+++ scripts/generate-test.sh
@@ -16,6 +16,11 @@ REPO_DIR="$(realpath "${SCRIPT_DIR}/..")"
 for DIR_PATH in "${REPO_DIR}/__tests__"/*; do
     DIR_NAME="`basename "${DIR_PATH}"`"
     BUNDLE="${REPO_DIR}/__tests__/${DIR_NAME}/${DIR_NAME}-bundle.js"
-    cat "${REPO_DIR}/__tests__/${DIR_NAME}/mocks.js" > "${BUNDLE}"
+    MOCKS_PATH="${REPO_DIR}/__tests__/${DIR_NAME}/mocks.js"
+
+    echo -n > "${BUNDLE}"
+    if [[ -f "${MOCKS_PATH}" ]]; then
+      cat "${REPO_DIR}/__tests__/${DIR_NAME}/mocks.js" >> "${BUNDLE}"
+    fi
     cat "${REPO_DIR}/src/${DIR_NAME}"/*\.js | sed 's/^function/export function/g' >> "${BUNDLE}"
 done
